23f99bd6c9d2423f8ff5e62a316b010df518b582
Xiaoyu Zhong <topaz.zhong@gmail.com>
2019-11-13 16:57:42 +0800

    Set custom ectd-manager image version

--- pkg/model/components/etcdmanager/model.go
+++ pkg/model/components/etcdmanager/model.go
@@ -190,7 +190,7 @@ metadata:
   namespace: kube-system
 spec:
   containers:
-  - image: kopeio/etcd-manager:3.0.20191025
+  - image: kopeio/etcd-manager:3.0.20191111
     name: etcd-manager
     resources:
       requests:
